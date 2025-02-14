a25706e24c33c7da562d100d43acf990b5f85068
Sadiya Gambo <prettysmiler6@gmail.com>
2025-02-14 07:15:57 -0500

    made changes

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
+const content = document.addEventListener('DOMContentLoaded', function () {
+    const form = document.getElementById('registration-form');
+    const feedbackDiv = document.getElementById('form-feedback');
 
+    form.addEventListener('click', function (event) {
+        event.preventDefault();
+    });
+
+    // retrieve userinput 
+    const username = document.getElementById('username');
+    username.value.trim();
+
+    const email = document.getElementById('email');
+    email.value.trim();
+
+    const password = document.getElementById('password');
+    password.value.trim();
+
+    // validation
+    let isValid = true;
+    let messages = [];
+
+    if(username.length < 3) {
+        isValid = false;
+        messages = 'Please enter the correct value!';
+    };
+
+    if (!email.includes('@', '.')) {
+        isValid = false;
+        messages = 'Must includes @ and .';
+    };
+
+    if(password.length < 8) {
+        isValid = false;
+        messages = 'Values must be 8 or more!';
+    };
+
+    // displaying feedback
+    feedbackDiv.style.display = "block";
+    if (isValid = true) {
+        feedbackDiv.textContent = "Registration successful!";
+        feedbackDiv.style.color = #28a745;
+    } else {
+        feedbackDiv.innerHTML= messages + "<br>";
+        feedbackDiv.style.color = #dc3545;
+    }
+
+});
